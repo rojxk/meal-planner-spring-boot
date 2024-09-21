734e605d0dbe1d329fd5cbeea4ae4a3c2acb57c1
Magda <magda.rojek2@gmail.com>
2024-09-21 21:19:24 +0200

    Update security config

--- src/main/java/com/mealplanner/config/SecurityConfig.java
+++ src/main/java/com/mealplanner/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.mealplanner.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager theUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        theUserDetailsManager
+                .setUsersByUsernameQuery("SELECT id, username, password, enabled FROM userdata WHERE id=?");
+
+        theUserDetailsManager
+                .setAuthoritiesByUsernameQuery("SELECT user_id, role FROM roles where user_id=?");
+
+        return theUserDetailsManager;
+    }
 
 }
