339f66a7a4743f76d294c08d036722fce5496ca2
Magda <magda.rojek2@gmail.com>
2024-09-21 21:06:14 +0200

    Add security config

---
+++ src/main/java/com/mealplanner/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.mealplanner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+}
